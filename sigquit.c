426cacbb0471910c41fc92fc0b6221a0e9856759
julienmeli <julienmeli@hotmail.com>
2025-04-10 15:53:34 +0200

    Add files via upload

--- sigquit.c
+++ sigquit.c
@@ -6,27 +6,23 @@
 /*   By: jmeli <jmeli@student.42luxembourg.lu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:06:37 by jmeli             #+#    #+#             */
-/*   Updated: 2025/04/10 15:12:31 by jmeli            ###   ########.fr       */
+/*   Updated: 2025/04/10 15:52:57 by jmeli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
+#include "../include/minishell.h"
 
 void	disable_ctrl_backslash_echo(void)
 {
 	struct termios	term;
 
-	// Get current terminal attributes
 	if (tcgetattr(STDIN_FILENO, &term) == -1)
 	{
 		perror("tcgetattr");
 		return ;
 	}
-	// Disable the quit character (Ctrl+\)
-	term.c_cc[VQUIT] = _POSIX_VDISABLE;
-	// Apply the modified terminal settings
+	// Disable ECHOCTL to stop printing ^\ for Ctrl+\*/
+	term.c_lflag &= ~ECHOCTL;
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &term) == -1)
 	{
 		perror("tcsetattr");
